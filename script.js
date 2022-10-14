0a0638c5a96773ed5acafb67263d179ed21534bf
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2022-10-15 00:32:09 +0100

    Done with level 300

--- script.js
+++ script.js
@@ -3,23 +3,29 @@
 const allEpisodes = getAllEpisodes();
 function setup() {
   getEpisodesFromURL();
-
-  //makePageForEpisodes(allEpisodes);
 }
 
 const rootElem = document.getElementById("root");
 
+// LEVEL 100   ------ EPISODE DISPLAY -----
+
 function makePageForEpisodes(episodeList) {
-  //getEpisodesFromURL();
   var ulEpisodes = document.createElement("ul");
   rootElem.appendChild(ulEpisodes);
   ulEpisodes.setAttribute("id", "myUL");
+
   episodeList.forEach((episode) => {
     var liEpisodes = document.createElement("li");
     liEpisodes.className = "listE";
+
+    //    FOR MEDIUM IMG
+
     var picEpisodes = document.createElement("img");
     picEpisodes.src = episode.image.medium;
     liEpisodes.appendChild(picEpisodes);
+
+    //    FOR SEASON AND EPISODE NUM
+
     var numEpisodes = document.createElement("h1");
     var numList = " ";
     if (episode.number < 10) {
@@ -27,17 +33,23 @@ function makePageForEpisodes(episodeList) {
     } else numList = `E${episode.number}`;
     numEpisodes.innerHTML = `S0${episode.season}` + "-" + numList;
     liEpisodes.appendChild(numEpisodes);
-    //
+
+    //    FOR EPISODE NAME
+
     var headingEpi = document.createElement("h2");
     headingEpi.innerHTML = episode.name;
     liEpisodes.appendChild(headingEpi);
-    //
+
+    //    FOR SUMMARY
+
     var summaryEpi = document.createElement("div");
     summaryEpi.innerHTML = episode.summary;
     liEpisodes.appendChild(summaryEpi);
     ulEpisodes.appendChild(liEpisodes);
   });
 }
+
+// LEVEL 200 ----- SEARCH BAR -----
 
 var inputs = document.getElementById("myInput");
 function searchEpisodes() {
@@ -50,9 +62,7 @@ function searchEpisodes() {
       );
     });
     rootElem.innerHTML = "";
-
     makePageForEpisodes(visibleEpi);
-
     var display = document.getElementById("display-label");
     display.innerHTML = "";
     display.innerHTML =
@@ -61,6 +71,8 @@ function searchEpisodes() {
 }
 
 searchEpisodes();
+
+// LEVEL 300 ----- DROPDOWN OPTIONS -----
 
 function dropdownEpi() {
   var divTop = document.getElementById("selectEpi");
